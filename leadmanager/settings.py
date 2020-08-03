f4be1c1110ed29a90c63f54b29cae6f907d720a9
Viktor Basharkevich <basharkevichv@gmail.com>
2020-08-02 22:05:48 -0400

    Added import dj_database_url

--- leadmanager/settings.py
+++ leadmanager/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'leadmanager.wsgi.application'
 #     }
 # }
 
-# import dj_database_url
+import dj_database_url
 #
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'] = dj_database_url.config(default='postgres://...'}
