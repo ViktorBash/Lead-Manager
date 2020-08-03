2f4e851a6a6a51dc88f4b49e0a8b39875ca4e70f
Viktor Basharkevich <basharkevichv@gmail.com>
2020-08-02 21:28:33 -0400

    Test

--- leadmanager/settings.py
+++ leadmanager/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'leadmanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "frontend")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
