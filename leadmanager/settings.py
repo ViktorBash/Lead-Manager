9da051e3ca5465572ad9c03665a32c139701dff3
ViktorB <basharkevichv@gmail.com>
2020-08-03 13:47:17 -0400

    Set debug to true to find internal server error

--- leadmanager/settings.py
+++ leadmanager/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Includes Heroku website and local
 ALLOWED_HOSTS = ["manage-leads.herokuapp.com", "192.168.1.53", "localhost", "127.0.0.1"]
